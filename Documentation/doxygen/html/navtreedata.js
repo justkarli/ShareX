13c305a36bce9ee09b35cdbf50ec388bacebf88f
JSimonjan <jennifer.simonjan@aau.at>
2015-04-26 00:11:24 +0200

    SOLID

---
+++ Documentation/doxygen/html/navtreedata.js
@@ -0,0 +1,12 @@
+var NAVTREE =
+[
+  [ "ShareX", "index.html", ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
