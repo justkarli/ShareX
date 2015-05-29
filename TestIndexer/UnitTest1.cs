ebf3005ab4a62d2660a689a337de607c07f157bd
justkarli <karli@counity.at>
2015-05-28 23:38:05 -0700

    Added IndexerLib class diagram and some TestIndexer schmafu

---
+++ TestIndexer/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestIndexer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
